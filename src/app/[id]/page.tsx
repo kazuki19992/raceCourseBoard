4553f7b6588f5182e65a3c0455b933f489e12f5a
kazuki19992 <kushida98@gmail.com>
2023-11-03 00:57:08 +0900

    スマホのみの対応

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -22,24 +22,31 @@ export default function RaceBoardPage() {
     last3: "34.7",
   };
   return (
-    <div className="aspect-[9/16] bg-black max-h-screen max-w-full text-white p-3">
-      <RaceInfo
-        raceCourseId={raceInfo.raceCourseId as 5}
-        raceNumber={raceInfo.raceNumber}
-        confirmedStatus={raceInfo.confirmedStatus}
-      />
-      <HorseNumbers
-        horseNumbers={horseNumbers.horseNumbers}
-        goalDiffs={horseNumbers.goalDiffs as GoalDiff[]}
-      />
-      <CourseTimeInfo
-        turf={0}
-        dirt={0}
-        timeInfo="record"
-        time={"1:55.2"}
-        last4="46.1"
-        last3="34.7"
-      />
+    <div className="w-full ">
+      <div className="aspect-[9/16] bg-black max-h-screen max-w-full text-white p-3 sm:hidden">
+        <RaceInfo
+          raceCourseId={raceInfo.raceCourseId as 5}
+          raceNumber={raceInfo.raceNumber}
+          confirmedStatus={raceInfo.confirmedStatus}
+        />
+        <HorseNumbers
+          horseNumbers={horseNumbers.horseNumbers}
+          goalDiffs={horseNumbers.goalDiffs as GoalDiff[]}
+        />
+        <CourseTimeInfo
+          turf={0}
+          dirt={0}
+          timeInfo="record"
+          time={"1:55.2"}
+          last4="46.1"
+          last3="34.7"
+        />
+      </div>
+      <div className="hidden sm:flex container mx-auto w-screen h-screen items-center justify-center">
+        <p className="text-white text-2xl">
+          このページはスマートフォンでのみ閲覧可能です
+        </p>
+      </div>
     </div>
   );
 }
