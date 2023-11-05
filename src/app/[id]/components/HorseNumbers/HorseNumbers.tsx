d9ecc52abdfa8efce2a3be62ca70e47ff2992240
kazuki19992 <kushida98@gmail.com>
2023-11-05 16:02:54 +0900

    コメントアウトを削除

--- src/app/[id]/components/HorseNumbers/HorseNumbers.tsx
+++ src/app/[id]/components/HorseNumbers/HorseNumbers.tsx
@@ -22,21 +22,12 @@ export const HorseNumbers: React.FC<{
             key={index}
           />
         ))}
-        {/* <HorseNumber goalNumber={1} horseNumber={7} />
-        <HorseNumber goalNumber={2} horseNumber={6} />
-        <HorseNumber goalNumber={3} horseNumber={9} />
-        <HorseNumber goalNumber={4} horseNumber={4} />
-        <HorseNumber goalNumber={5} horseNumber={5} /> */}
       </div>
       <div className="w-1/2 gap-4 flex flex-col">
         {/* 着順馬番 */}
         {goalDiffs.map((goalDiff, index) => (
           <GoalDiff goalDiff={goalDiff} key={index} />
         ))}
-        {/* <GoalDiff goalDiff={GoalDiffType.diff2_1_2} />
-        <GoalDiff goalDiff={GoalDiffType.diff1_1_4} />
-        <GoalDiff goalDiff={GoalDiffType.neck} />
-        <GoalDiff goalDiff={GoalDiffType.diff2_1_2} /> */}
       </div>
     </div>
   );
